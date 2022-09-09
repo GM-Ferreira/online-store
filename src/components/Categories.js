b23421d34df2b0e0ef92e62da8755a2d56fb3f49
Pedro Kotzias <pepeuk@gmail.com>
2022-09-09 12:05:52 -0300

    Req 6 completo, observação: provavelmente problema de sincronia

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import { getCategories } from '../services/api';
 
 class Categories extends React.Component {
@@ -14,20 +16,37 @@ class Categories extends React.Component {
     });
   }
 
+  handleChange = async (event) => {
+    const { getCategory, doSearch } = this.props;
+    getCategory(event);
+    const { target: { value } } = event;
+    await doSearch(value);
+  };
+
   render() {
     const { categories } = this.state;
-
     return (
       <div>
         {categories.map((category) => (
           <label htmlFor={ category.id } data-testid="category" key={ category.id }>
             {category.name}
-            <input type="radio" id={ category.id } name="category" />
+            <input
+              value={ category.id }
+              type="radio"
+              id={ category.id }
+              name="category"
+              onChange={ (e) => this.handleChange(e) }
+            />
           </label>
         ))}
       </div>
     );
   }
 }
+
+Categories.propTypes = {
+  getCategory: PropTypes.func.isRequired,
+  doSearch: PropTypes.func.isRequired,
+};
 
 export default Categories;
--- src/services/api.js
+++ src/services/api.js
@@ -10,7 +10,20 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   return data;
 }
 
-export async function getProductById() {
-  // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
-  // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
+export async function getProductsFromQuery(query) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function getProductsFromCategory(categoryId) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function getProductById(productId) {
+  const response = await fetch(`https://api.mercadolibre.com/items/${productId}`);
+  const data = await response.json();
+  return data;
 }

9a4590435acda295e97b49a70a8658d08fad58c2
Aline Grance <grance.aline@gmail.com>
2022-09-13 15:58:12 -0300

    Req12 iniciado

---
+++ src/pages/Checkout.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import CheckoutForm from '../components/CheckoutForm';
+
+export default class Checkout extends React.Component {
+  state = {
+    cart: [],
+    cartSet: [],
+  };
+
+  componentDidMount() {
+    const data = JSON.parse(localStorage.getItem('cartList'));
+    const dataId = data?.map((item) => item.id);
+    const setId = new Set(dataId);
+    this.setState({
+      cart: data,
+      cartSet: [...setId],
+    });
+  }
+
+  render() {
+    const { cart, cartSet } = this.state;
+    return (
+      <>
+        <ul>
+          {
+            cartSet.map((id) => {
+              const item = cart.find((e) => e.id === id);
+              return (
+                <li key={ item.id }>{item.title}</li>
+              );
+            })
+          }
+        </ul>
+        <CheckoutForm />
+      </>
+    );
+  }
+}
